1828cf32eba4c1eeb974d794329c4afb7f35c31a
mskalisz <mskalisz3@gmail.com>
2021-09-01 15:27:47 -0400

    Add prompt to exericse 2

---
+++ oop_inheritance/exercise_2.rb
@@ -0,0 +1,4 @@
+=begin
+Add a class variable to your superclass that can keep track of the number of objects created that inherit from the superclass. 
+Create a method to print out the value of this class variable as well.
+=end
