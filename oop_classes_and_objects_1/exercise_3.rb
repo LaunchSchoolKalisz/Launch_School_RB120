6a4281be175d218c3953676523f127861786b0ed
mskalisz <mskalisz3@gmail.com>
2021-09-01 13:37:13 -0400

    Add LS Solution

--- oop_classes_and_objects_1/exercise_3.rb
+++ oop_classes_and_objects_1/exercise_3.rb
@@ -48,3 +48,18 @@ end
 
 lumina = MyCar.new(1997, 'chevy lumina', 'white')
 lumina.spray_paint('red')
+
+# LS Solution
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+
+  # ... rest of class left out for brevity
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+lumina.spray_paint('red')   #=> "Your new red paint job looks great!"
