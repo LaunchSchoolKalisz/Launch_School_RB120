01538187ebc00d2b51fc56a0bbe88fae416be843
mskalisz <mskalisz3@gmail.com>
2021-09-01 14:40:06 -0400

    Solve exercise 2

--- oop_classes_and_objects_2/exercise_2.rb
+++ oop_classes_and_objects_2/exercise_2.rb
@@ -2,3 +2,51 @@
 Override the to_s method to create a user friendly print out of your object.
 =end
 
+class MyCar
+
+  def self.gas_mileage(gallons, miles)
+    puts "This car gets #{miles / gallons} mpg!" 
+  end
+  
+  def to_s
+    "My car is a #{@color}, #{@year}, #{@model}"
+  end
+
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color # can use self.color because we defined the setter method above
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+puts lumina
+MyCar.gas_mileage(1, 15)
