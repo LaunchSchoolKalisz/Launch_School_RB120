fd1f4a0536e3180ab517d05fa38c1925599947c6
mskalisz <mskalisz3@gmail.com>
2021-09-01 14:52:33 -0400

    Add LS solution exercise 3

--- oop_classes_and_objects_2/exercise_3.rb
+++ oop_classes_and_objects_2/exercise_3.rb
@@ -30,3 +30,22 @@ end
 
 bob = Person.new("Steve")
 bob.name = "Bob"
+
+=begin 
+LS Solution
+
+We get this error because attr_reader only creates a getter method. When we try to reassign the name instance variable to "Bob", we 
+need a setter method called name=. We can get this by changing attr_reader to attr_accessor or attr_writer if we don't intend to use 
+the getter functionality.
+
+class Person
+  attr_accessor :name
+  # attr_writer :name ## => This also works but doesn't allow getter access
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new("Steve")
+bob.name = "Bob"
+=end
