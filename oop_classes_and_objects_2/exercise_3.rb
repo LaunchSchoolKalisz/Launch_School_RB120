78e060d58a3f1e24da63400adbafe62d994a9d40
mskalisz <mskalisz3@gmail.com>
2021-09-01 14:50:57 -0400

    Solve exercise 3

--- oop_classes_and_objects_2/exercise_3.rb
+++ oop_classes_and_objects_2/exercise_3.rb
@@ -15,6 +15,18 @@ We get the following error...
 
 test.rb:9:in `<main>': undefined method `name=' for
   #<Person:0x007fef41838a28 @name="Steve"> (NoMethodError)
-  
+
 Why do we get this error and how do we fix it?
+  We get this error because we used attr_reader which only gives us a getter method, and we are trying to set the name
+  We can add attr_writer or attr_accessor
 =end
+
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new("Steve")
+bob.name = "Bob"
