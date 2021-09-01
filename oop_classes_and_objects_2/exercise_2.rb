97ef33c1a5db752908e910c4bf22f9c57e2ecafc
mskalisz <mskalisz3@gmail.com>
2021-09-01 14:40:51 -0400

    Add LS solution

--- oop_classes_and_objects_2/exercise_2.rb
+++ oop_classes_and_objects_2/exercise_2.rb
@@ -50,3 +50,18 @@ end
 lumina = MyCar.new(1997, 'chevy lumina', 'white')
 puts lumina
 MyCar.gas_mileage(1, 15)
+
+#LS Solution
+
+class MyCar
+  # code omitted for brevity...
+
+  def to_s
+    "My car is a #{color}, #{year}, #{@model}!"
+  end
+end
+
+my_car = MyCar.new("2010", "Ford Focus", "silver")
+puts my_car  # => My car is a silver, 2010, Ford Focus.
+
+## Note the "puts" calls "to_s" automatically.
