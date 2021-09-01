964182fe60d5bbe64e71c417b26cf1af3bfaf9c1
mskalisz <mskalisz3@gmail.com>
2021-09-01 14:26:43 -0400

    Add LS solution exercise 1

--- oop_classes_and_objects_2/exercise_1.rb
+++ oop_classes_and_objects_2/exercise_1.rb
@@ -45,3 +45,16 @@ end
 
 lumina = MyCar.new(1997, 'chevy lumina', 'white')
 MyCar.gas_mileage(15, 1)
+
+# LS Solution
+
+class MyCar
+
+  # code omitted for brevity...
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+MyCar.gas_mileage(13, 351)  # => "27 miles per gallon of gas"
