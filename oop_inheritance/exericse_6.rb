0a168f915b74d6b09255990e7ceadec13d682510
mskalisz <mskalisz3@gmail.com>
2021-09-01 15:32:48 -0400

    Add prompt exercise 7

--- oop_inheritance/exericse_6.rb
+++
@@ -1,4 +0,0 @@
-=begin
-Write a method called age that calls a private method to calculate the age of the vehicle. Make sure the private method is not available 
-from outside of the class. You'll need to use Ruby's built-in Time class to help.
-=end
