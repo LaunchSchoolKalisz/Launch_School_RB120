8211d3067bd4e5a3af358e24e6df8063f5c4c05e
mskalisz <mskalisz3@gmail.com>
2021-09-01 15:30:10 -0400

    Add prompt for exercise 5

---
+++ oop_inheritance/exercise_5.rb
@@ -0,0 +1,4 @@
+=begin
+Move all of the methods from the MyCar class that also pertain to the MyTruck class into the Vehicle class. 
+Make sure that all of your previous method calls are working when you are finished.
+=end
