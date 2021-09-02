6538cfaf93b2c06f970af32e30b89f02a732db51
mskalisz <mskalisz3@gmail.com>
2021-09-01 22:58:28 -0400

    Solve exercise 2

--- oop_inheritance/exercise_2.rb
+++ oop_inheritance/exercise_2.rb
@@ -2,3 +2,60 @@
 Add a class variable to your superclass that can keep track of the number of objects created that inherit from the superclass. 
 Create a method to print out the value of this class variable as well.
 =end
+
+class Vehicle
+
+  @@number_of_vehicles = 0
+
+  def initialize
+    @@number_of_vehicles += 1
+  end
+
+  def self.total_number_of_vehicles
+    @@number_of_vehicles
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "This vehicle gets #{miles / gallons} mpg!" 
+  end
+  
+  def to_s
+    "My vehicle is a #{@color}, #{@year}, #{@model}"
+  end
+
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color # can use self.color because we defined the setter method above
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+#
