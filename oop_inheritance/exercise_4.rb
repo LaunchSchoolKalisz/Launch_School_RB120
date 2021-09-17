3477a28094421603ea9e5de75e82106f3fb70822
mskalisz <mskalisz3@gmail.com>
2021-09-17 18:23:39 -0400

    Solve exericse 4

--- oop_inheritance/exercise_4.rb
+++ oop_inheritance/exercise_4.rb
@@ -1,3 +1,73 @@
 =begin
 Print to the screen your method lookup for the classes that you have created.
 =end
+
+module Offroadable
+  "I can drive over rocks"
+end
+
+class Vehicle
+
+  attr_accessor :color
+  attr_reader :year
+
+  @@number_of_vehicles = 0
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+    @@number_of_vehicles += 1
+  end
+
+  def self.total_number_of_vehicles
+    @@number_of_vehicles
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "This vehicle gets #{miles / gallons} mpg!" 
+  end
+  
+  def to_s
+    "My vehicle is a #{@color}, #{@year}, #{@model}"
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color # can use self.color because we defined the setter method above
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_SEATS = 5
+end
+
+class MyTruck < Vehicle
+  NUMBER_OF_SEATS = 8
+
+  include Offroadable
+end
+
+puts MyTruck.ancestors
+puts MyCar.ancestors
+puts Vehicle.ancestors
