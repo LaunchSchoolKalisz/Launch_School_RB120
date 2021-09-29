7dbb65e6be2cc505d4328cbec63b22d38c5ff3eb
mskalisz <mskalisz3@gmail.com>
2021-09-29 07:23:26 -0400

    Add LS solution

--- lesson_2/example_1.rb
+++ lesson_2/example_1.rb
@@ -10,3 +10,15 @@ bob = Person.new('bob')
 p bob.name                  # => 'bob'
 bob.name = 'Robert'
 p bob.name                  # => 'Robert'
+
+# LS solution
+
+#A nice, simple class with one getter/setter.
+
+class Person
+  attr_accessor :name
+
+  def initialize(n)
+    @name = n
+  end
+end
