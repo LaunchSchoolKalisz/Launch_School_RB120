1746d90a9f5d748b2dfe88a4675c970ecd7493c7
mskalisz <mskalisz3@gmail.com>
2021-09-30 09:13:59 -0400

    Solve example 5

--- lesson_2/example_5.rb
+++ lesson_2/example_5.rb
@@ -3,4 +3,33 @@ Continuing with our Person class definition, what does the below print out?
 
 bob = Person.new("Robert Smith")
 puts "The person's name is: #{bob}"
+
+Will print the person object which will not look very user friendly
 =end
+
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parse_full_name(full_name)
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name)
+    parse_full_name(full_name)
+  end
+
+  private
+
+  def parse_full_name(full_name)
+    parts = full_name.split
+    self.first_name = parts.first
+    self.last_name = parts.size > 1 ? parts.last : ''
+  end
+end
+
+bob = Person.new("Robert Smith")
+puts "The person's name is: #{bob}"
