d840f12e69661014ae921e9f179f6a471673ab64
mskalisz <mskalisz3@gmail.com>
2021-09-30 10:58:45 -0400

    Add LS solution for what are you

--- lesson_2/oo_basics_classes_objects_1/4_what_are_you.rb
+++ lesson_2/oo_basics_classes_objects_1/4_what_are_you.rb
@@ -21,3 +21,23 @@ class Cat
 end
 
 kitty = Cat.new
+
+=begin
+LS Solution
+
+class Cat
+  def initialize
+    puts "I'm a cat!"
+  end
+end
+
+kitty = Cat.new
+
+Discussion
+When defining a class, you usually need to define the #initialize method. #initialize is useful for setting up 
+-- initializing -- an object so the object is ready for immediate use.
+
+To add #initialize to Cat, define it with the reserved word def. Adding this method lets us execute certain 
+statements when a new Cat object is initialized. In this case, we want to print I'm a cat!. We accomplish 
+this by using #puts.
+=end
