f0565873fce3b28e63cab2d47629d788f36d8875
mskalisz <mskalisz3@gmail.com>
2021-09-30 13:47:44 -0400

    Solve walk the cat

--- lesson_2/oo_basics_classes_objects_1/10_walk_the_cat.rb
+++ lesson_2/oo_basics_classes_objects_1/10_walk_the_cat.rb
@@ -23,3 +23,26 @@ Hello! My name is Sophie!
 Let's go for a walk!
 =end
 
+module Walkable
+  def walk
+    puts "Let's go for a walk!"
+  end
+end
+
+class Cat
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.walk
