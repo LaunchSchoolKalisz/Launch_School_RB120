2230bb6a9e8c4421457ab000ae9b263d24eaec4c
mskalisz <mskalisz3@gmail.com>
2021-09-30 09:25:33 -0400

    Solve example 5

--- lesson_2/example_5.rb
+++ lesson_2/example_5.rb
@@ -4,7 +4,25 @@ Continuing with our Person class definition, what does the below print out?
 bob = Person.new("Robert Smith")
 puts "The person's name is: #{bob}"
 
-Will print the person object which will not look very user friendly
+Will print the person object which will not look very user friendly:
+  The person's name is: #<Person:0x00007fd2a495a488>
+
+Let's add a to_s method to the class:
+
+class Person
+  # ... rest of class omitted for brevity
+
+  def to_s
+    name
+  end
+end
+Now, what does the below output?
+
+YAY! Looks way better now:
+  The person's name is: Robert Smith
+
+bob = Person.new("Robert Smith")
+puts "The person's name is: #{bob}"
 =end
 
 class Person
@@ -20,6 +38,10 @@ class Person
 
   def name=(full_name)
     parse_full_name(full_name)
+  end
+
+  def to_s
+    name
   end
 
   private
