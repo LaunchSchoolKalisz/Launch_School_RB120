f44e1fe1a73031e996369bda5f1df597c4741dfa
mskalisz <mskalisz3@gmail.com>
2021-09-30 08:39:59 -0400

    Add LS solution exercise 3

--- lesson_2/example_3.rb
+++ lesson_2/example_3.rb
@@ -45,3 +45,60 @@ p bob.name = "John Adams"
 p bob.first_name            # => 'John'
 p bob.last_name             # => 'Adams'
 
+=begin
+LS solution
+
+We actually already did this in the initialize method, so we can just repeat it for the 
+name= method. Note that Ruby gives us a special syntax for methods that end in =, so the 
+name= method can be used like this: bob.name = 'Robert Smith', rather than the more 
+awkward bob.name=('Robert Smith').
+
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name)
+    parts = full_name.split
+    self.first_name = parts.first
+    self.last_name = parts.size > 1 ? parts.last : ''
+  end
+end
+
+Note the redundant code in the initialize and the name= methods. Let's move that to another 
+method to DRY up the code. Our new method can be private, since it's not being used outside of 
+the class definition.
+
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parse_full_name(full_name)
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name)
+    parse_full_name(full_name)
+  end
+
+  private
+
+  def parse_full_name(full_name)
+    parts = full_name.split
+    self.first_name = parts.first
+    self.last_name = parts.size > 1 ? parts.last : ''
+  end
+end
+
+=end
