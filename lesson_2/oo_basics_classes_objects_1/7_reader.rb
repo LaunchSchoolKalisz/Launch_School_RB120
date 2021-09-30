a9ddfdc4ce8f79131a3af2007755172230033d45
mskalisz <mskalisz3@gmail.com>
2021-09-30 13:16:59 -0400

    Solve reader

--- lesson_2/oo_basics_classes_objects_1/7_reader.rb
+++ lesson_2/oo_basics_classes_objects_1/7_reader.rb
@@ -19,3 +19,18 @@ kitty.greet
 Expected output:
 Hello! My name is Sophie!
 =end
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
