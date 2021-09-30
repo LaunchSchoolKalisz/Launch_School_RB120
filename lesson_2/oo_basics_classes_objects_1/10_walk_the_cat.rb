0a53d22f31936e5fcfb50b10023340dc9617e77e
mskalisz <mskalisz3@gmail.com>
2021-09-30 13:49:00 -0400

    Add LS solution for walk the cat

--- lesson_2/oo_basics_classes_objects_1/10_walk_the_cat.rb
+++ lesson_2/oo_basics_classes_objects_1/10_walk_the_cat.rb
@@ -46,3 +46,51 @@ end
 kitty = Cat.new('Sophie')
 kitty.greet
 kitty.walk
+
+=begin
+LS Solution
+
+module Walkable
+  def walk
+    puts "Let's go for a walk!"
+  end
+end
+
+class Cat
+  include Walkable
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.walk
+
+Discussion
+Modules are typically used to contain methods that may be useful for multiple classes, but not all classes. 
+When you mix a module into a class, you're allowing the class to invoke the contained methods.
+
+In our solution, we create a module named Walkable that contains a method named #walk. We give Cat access to 
+this method by including Walkable in the class, like this:
+
+module Walkable
+  def walk
+    puts "Let's go for a walk!"
+  end
+end
+
+class Cat
+  include Walkable
+end
+
+This lets us invoke #walk on any instance of Cat. In this case, if we invoke kitty.walk, then Let's go for a 
+walk! will be printed.
+=end
