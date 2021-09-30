6ca984530d592465ee5c6282c2e5851d9b7f9420
mskalisz <mskalisz3@gmail.com>
2021-09-30 14:06:37 -0400

    Add prompt for generic greeting

---
+++ lesson_2/oo_basics_classes_objects_2/1_generic_greeting_1.rb
@@ -0,0 +1,11 @@
+=begin
+Modify the following code so that Hello! I'm a cat! is printed when Cat.generic_greeting is invoked.
+
+class Cat
+end
+
+Cat.generic_greeting
+Expected output:
+
+Hello! I'm a cat!
+=end
