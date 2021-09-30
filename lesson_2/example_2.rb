1bfb66dd0cf5086a4d31d6cf02e98b42df5ae0a4
mskalisz <mskalisz3@gmail.com>
2021-09-30 08:12:36 -0400

    Add solution for example 2 with LS Solution

--- lesson_2/example_2.rb
+++ lesson_2/example_2.rb
@@ -12,3 +12,41 @@ bob.name                  # => 'Robert Smith'
 Hint: let first_name and last_name be "states" and create an instance method called name that 
 uses those states.
 =end
+
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts[0]
+    @last_name =  parts.size > 1 ? parts.last : ''
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+end
+
+bob = Person.new('Robert')
+p bob.name                  # => 'Robert'
+p bob.first_name            # => 'Robert'
+p bob.last_name             # => ''
+p bob.last_name = 'Smith'
+p bob.name                  # => 'Robert Smith'
+
+# LS Solution
+#This class has two getter/setter pairs, and a regular instance method called name.
+
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+end
