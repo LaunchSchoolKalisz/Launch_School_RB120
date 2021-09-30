28f1cbf90fa1acb412ee3bb708e1a077cb82d4ee
mskalisz <mskalisz3@gmail.com>
2021-09-30 13:23:38 -0400

    Solve writer

--- lesson_2/oo_basics_classes_objects_1/8_writer.rb
+++ lesson_2/oo_basics_classes_objects_1/8_writer.rb
@@ -23,3 +23,21 @@ Expected output:
 Hello! My name is Sophie!
 Hello! My name is Luna!
 =end
+
+class Cat
+  attr_reader :name
+  attr_writer :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.name=('Luna')
+kitty.greet
