a745e312d58fa5ab4e281549d9888670f4d0221d
mskalisz <mskalisz3@gmail.com>
2021-09-30 21:05:20 -0400

    Add LS solution identify yourself 1

--- lesson_2/oo_basics_classes_objects_2/3_identify_yourself_1.rb
+++ lesson_2/oo_basics_classes_objects_2/3_identify_yourself_1.rb
@@ -31,3 +31,33 @@ end
 
 kitty = Cat.new('Sophie')
 p kitty.identify
+
+=begin
+LS Solution
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def identify
+    self
+  end
+end
+
+kitty = Cat.new('Sophie')
+p kitty.identify
+
+Discussion
+In the last two exercises, we used self to define class methods and to modify instance variables. self is 
+used because it refers to the calling object. This means that, in our solution, invoking self is the same 
+as invoking kitty.
+
+p kitty.identify # => #<Cat:0x007f932b06dba8 @name="Sophie">
+p kitty          # => #<Cat:0x007f932b06dba8 @name="Sophie">
+
+We use #p to print the object so that #inspect is called, which lets us view the instance variables and their 
+values along with the object.
+=end
