2bbde30b8e989375826d189e317ceffa9ebd7755
mskalisz <mskalisz3@gmail.com>
2021-10-01 14:41:42 -0400

    Add LS solution generic greeting 2

--- lesson_2/oo_basics_classes_objects_2/4_generic_greeting_2.rb
+++ lesson_2/oo_basics_classes_objects_2/4_generic_greeting_2.rb
@@ -41,3 +41,43 @@ kitty = Cat.new('Sophie')
 
 Cat.generic_greeting
 kitty.personal_greeting
+
+=begin
+LS Solution
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.generic_greeting
+    puts "Hello! I'm a cat!"
+  end
+
+  def personal_greeting
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+
+Cat.generic_greeting
+kitty.personal_greeting
+
+Discussion
+There are a few differences between class methods and instance methods, however, most notably, class methods 
+are only associated with the class itself, not any instance of the class. Within class methods, we're 
+restricted from adding data specific to objects of the class, like adding a name to an instance of Cat. 
+Think of class methods as generic actions a class may perform, like this:
+
+class Cat
+  def self.speak
+    puts 'Meow!'
+  end
+end
+
+Cat.speak # => Meow!
+We know that cats meow, therefore, it makes sense to add #speak as a generic action for Cat.
+=end
