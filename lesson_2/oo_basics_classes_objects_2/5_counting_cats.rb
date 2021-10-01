b2140949e137654f237d6267e3ede45bf0d8bf02
mskalisz <mskalisz3@gmail.com>
2021-10-01 15:27:22 -0400

    Solve counting cats

--- lesson_2/oo_basics_classes_objects_2/5_counting_cats.rb
+++ lesson_2/oo_basics_classes_objects_2/5_counting_cats.rb
@@ -11,3 +11,20 @@ Expected output:
 
 2
 =end
+
+class Cat
+  @@number = 0
+
+  def initialize
+    @@number += 1
+  end
+
+  def self.total
+    p @@number
+  end
+end
+
+kitty1 = Cat.new
+kitty2 = Cat.new
+
+Cat.total
