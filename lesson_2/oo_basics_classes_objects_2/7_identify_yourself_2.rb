e657789d6c728c9b8ee64e5f22ce63e4b11e90b7
mskalisz <mskalisz3@gmail.com>
2021-10-03 15:12:44 -0400

    Add LS Solution for identify yourself 2

--- lesson_2/oo_basics_classes_objects_2/7_identify_yourself_2.rb
+++ lesson_2/oo_basics_classes_objects_2/7_identify_yourself_2.rb
@@ -32,3 +32,29 @@ end
 
 kitty = Cat.new('Sophie')
 puts kitty
+
+=begin
+LS Solution
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "I'm #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+puts kitty
+
+Discussion
+The key for this exercise is that every class has a #to_s method. puts calls #to_s method for every argument 
+it gets passed to convert the value to an appropriate string representation. Knowing this, we can override 
+#to_s for any class to produce any useful output we need.
+
+Here, we define #to_s to return I'm Sophie!, so puts kitty prints I'm Sophie!.
+=end
