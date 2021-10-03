08537e7496de5ce6e11b9de22b5f2da384f729fc
mskalisz <mskalisz3@gmail.com>
2021-10-03 15:10:11 -0400

    Solve identify yourself 2

--- lesson_2/oo_basics_classes_objects_2/7_identify_yourself_2.rb
+++ lesson_2/oo_basics_classes_objects_2/7_identify_yourself_2.rb
@@ -1,7 +1,6 @@
 =begin
 Update the following code so that it prints I'm Sophie! when it invokes puts kitty.
 
-Copy Code
 class Cat
   attr_reader :name
 
@@ -12,8 +11,24 @@ end
 
 kitty = Cat.new('Sophie')
 puts kitty
+
 Expected output:
 
-Copy Code
 I'm Sophie!
 =end
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "I'm #{@name}"
+  end
+
+end
+
+kitty = Cat.new('Sophie')
+puts kitty
