1773fc0181ec9684ccc8d3f36c4cf453a65985a3
mskalisz <mskalisz3@gmail.com>
2021-10-04 16:42:33 -0400

    Add LS solution public secret

--- lesson_2/oo_basics_classes_objects_2/8_public_secret.rb
+++ lesson_2/oo_basics_classes_objects_2/8_public_secret.rb
@@ -26,3 +26,32 @@ end
 person1 = Person.new
 person1.secret = 'Shh.. this is a secret!'
 puts person1.secret
+
+=begin
+LS Solution
+
+class Person
+  attr_accessor :secret
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+puts person1.secret
+Discussion
+All of the instance methods used in the exercises so far have been public methods. Public methods can be 
+accessed from both within the class and outside the class.
+
+In the solution, we use attr_accessor to automatically create the getter and setter methods. They can also be 
+written manually, like this:
+
+class Person
+  def secret
+    @secret
+  end
+
+  def secret=(value)
+    @secret = value
+  end
+end
+
+=end
