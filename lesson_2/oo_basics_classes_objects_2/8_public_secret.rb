084926b1c8577c9e1ebb3b730137d8dacc1a2f2c
mskalisz <mskalisz3@gmail.com>
2021-10-04 16:40:17 -0400

    Solve public secret

--- lesson_2/oo_basics_classes_objects_2/8_public_secret.rb
+++ lesson_2/oo_basics_classes_objects_2/8_public_secret.rb
@@ -10,3 +10,19 @@ Expected output:
 
 Shh.. this is a secret!
 =end
+
+class Person
+  attr_accessor :secret
+
+  def initialize
+    @secret = secret
+  end
+
+  def to_s 
+    'Shh.. this is a secret!'
+  end
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+puts person1.secret
