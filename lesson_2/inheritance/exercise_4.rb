6b4ffcffd8632c83e9d95fe97102f2174e5d6cb9
mskalisz <mskalisz3@gmail.com>
2021-10-05 22:33:32 -0400

    Solve exercise 4

--- lesson_2/inheritance/exercise_4.rb
+++ lesson_2/inheritance/exercise_4.rb
@@ -1,4 +1,7 @@
 =begin
 What is the method lookup path and how is it important?
 
+The method lookup path is the order in which classes are searched for their methods and in what order
+This is important so that you know if a method with tthe same name will be called or overridden by 
+a previously called method
 =end
