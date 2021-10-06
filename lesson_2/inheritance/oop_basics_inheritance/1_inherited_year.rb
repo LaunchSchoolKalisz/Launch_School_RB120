04cf10cd010ac0293a1973285e311f478568a4d9
mskalisz <mskalisz3@gmail.com>
2021-10-06 06:13:25 -0400

    Solve inherited year

--- lesson_2/inheritance/oop_basics_inheritance/1_inherited_year.rb
+++ lesson_2/inheritance/oop_basics_inheritance/1_inherited_year.rb
@@ -20,3 +20,23 @@ Expected output:
 1994
 2006
 =end
+
+class Vehicle
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+end
+
+class Truck < Vehicle
+end
+
+class Car < Vehicle
+end
+
+truck1 = Truck.new(1994)
+puts truck1.year
+
+car1 = Car.new(2006)
+puts car1.year
