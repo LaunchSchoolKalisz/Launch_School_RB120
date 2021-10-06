2bd867444e5a16fcf712415e8b70ed90e2a7f650
mskalisz <mskalisz3@gmail.com>
2021-10-05 22:18:32 -0400

    Add prompt for exercise 2

---
+++ lesson_2/inheritance/exercise_2.rb
@@ -0,0 +1,29 @@
+=begin
+Let's create a few more methods for our Dog class.
+
+class Dog
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+end
+
+Create a new class called Cat, which can do everything a dog can, except swim or fetch. Assume the methods 
+do the exact same thing. Hint: don't just copy and paste all methods in Dog into Cat; try to come up 
+with some class hierarchy.
+=end
