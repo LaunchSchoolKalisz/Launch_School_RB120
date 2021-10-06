9a086f9b58df82fce9c869a652e9dea9efb2953a
mskalisz <mskalisz3@gmail.com>
2021-10-06 06:15:07 -0400

    Add LS solution for inherited year

--- lesson_2/inheritance/oop_basics_inheritance/1_inherited_year.rb
+++ lesson_2/inheritance/oop_basics_inheritance/1_inherited_year.rb
@@ -40,3 +40,36 @@ puts truck1.year
 
 car1 = Car.new(2006)
 puts car1.year
+
+=begin
+LS Solution
+
+class Vehicle
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+end
+
+class Truck < Vehicle
+end
+
+class Car < Vehicle
+end
+
+truck1 = Truck.new(1994)
+puts truck1.year
+
+car1 = Car.new(2006)
+puts car1.year
+
+Discussion
+When designing an Object Oriented program, it's common to have multiple classes that perform similar actions. 
+For example, both Truck and Car use the instance variable @year. To reduce complexity, classes with similar 
+behaviors can inherit from a superclass. The superclass implements the common behaviors while the inheriting 
+classes invoke them.
+
+In the solution, Truck and Car inherit @year from Vehicle. The < symbol is used to denote inheritance between 
+classes.
+=end
