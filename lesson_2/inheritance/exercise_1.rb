7ee279ad9c4dd7a43ed1aa288c186e225d7f720f
mskalisz <mskalisz3@gmail.com>
2021-10-05 22:13:23 -0400

    Solve exercise 1

--- lesson_2/inheritance/exercise_1.rb
+++ lesson_2/inheritance/exercise_1.rb
@@ -21,3 +21,23 @@ behaviors. For example, bulldogs can't swim, but all other dogs can.
 Create a sub-class from Dog called Bulldog overriding the swim method to return "can't swim!"
 
 =end
+
+class Dog
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  class Bulldog
+    def swim
+      "can't swim!"
+    end
+  end
+end
+
+teddy = Dog.new
+puts teddy.speak           # => "bark!"
+puts teddy.swim           # => "swimming!"
