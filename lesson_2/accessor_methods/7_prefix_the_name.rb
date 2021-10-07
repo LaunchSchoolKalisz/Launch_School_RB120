82daa921025b41b261a4f7015fada8e70a2dd823
mskalisz <mskalisz3@gmail.com>
2021-10-07 12:32:01 -0400

    Add LS solution for prefix the name

--- lesson_2/accessor_methods/7_prefix_the_name.rb
+++ lesson_2/accessor_methods/7_prefix_the_name.rb
@@ -25,3 +25,28 @@ end
 person1 = Person.new
 person1.name = 'James'
 puts person1.name
+
+=begin
+LS solution
+
+class Person
+  attr_writer :name
+
+  def name
+    "Mr. #{@name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'James'
+puts person1.name
+
+Discussion
+In the previous exercise, we formatted the data upon assignment. In this example, if we prefixed @name upon 
+assignment, the value would be modified, which may not be what we want. The goal is to return the value of 
+@name, but with a prefix.
+
+To accomplish this, we need to manually implement the getter method. As mentioned in the last exercise, we 
+can't use the built in accessor methods because we need extra functionality. Therefore, to return the 
+prefixed @name, we simply need to add the desired prefix, and add @name through string interpolation.
+=end
