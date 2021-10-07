a0ef561b25549fb89bd1198b672509fb803bdd68
mskalisz <mskalisz3@gmail.com>
2021-10-07 11:09:18 -0400

    Add LS solution to access denied

--- lesson_2/accessor_methods/3_access_denied.rb
+++ lesson_2/accessor_methods/3_access_denied.rb
@@ -35,3 +35,31 @@ puts person1.phone_number
 
 person1.phone_number = 9987654321
 puts person1.phone_number
+
+=begin
+LS Solution
+
+class Person
+  attr_reader :phone_number
+
+  def initialize(number)
+    @phone_number = number
+  end
+end
+
+person1 = Person.new(1234567899)
+puts person1.phone_number
+
+person1.phone_number = 9987654321
+puts person1.phone_number
+
+Discussion
+When adding getters and setters, it's easy to get carried away and simply add #attr_accessor for every instance
+variable. However, doing this can have negative implications. In the initial example, #attr_accessor is used 
+for @phone_number. This means that @phone_number can be modified from outside the class, which we don't want.
+
+We need to ensure that @phone_number cannot be modified from outside the class. To do this, we simply need to 
+remove the setter method by changing #attr_accessor to #attr_reader. This still lets us set the value of the 
+@phone_number instance variable when instantiating the object, and lets us read its value from outside of the 
+class, but doesn't let us modify it from outside the class.
+=end
