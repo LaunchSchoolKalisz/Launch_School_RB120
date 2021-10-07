33e943abca3b79efc53ab3817582a7a64c21abf1
mskalisz <mskalisz3@gmail.com>
2021-10-07 11:08:11 -0400

    Solve access denied

--- lesson_2/accessor_methods/3_access_denied.rb
+++ lesson_2/accessor_methods/3_access_denied.rb
@@ -21,3 +21,17 @@ Expected output:
 1234567899
 NoMethodError
 =end
+
+class Person
+  attr_reader :phone_number
+
+  def initialize(number)
+    @phone_number = number
+  end
+end
+
+person1 = Person.new(1234567899)
+puts person1.phone_number
+
+person1.phone_number = 9987654321
+puts person1.phone_number
