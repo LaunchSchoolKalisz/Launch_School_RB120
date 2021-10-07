61ebad036a0c16ce533c2867212e11ecbc6f60ea
mskalisz <mskalisz3@gmail.com>
2021-10-07 12:30:17 -0400

    Solve prefix the name

--- lesson_2/accessor_methods/7_prefix_the_name.rb
+++ lesson_2/accessor_methods/7_prefix_the_name.rb
@@ -13,3 +13,15 @@ Expected output:
 
 Mr. James
 =end
+
+class Person
+  attr_reader :name
+
+  def name=(name)
+    @name = "Mr. #{name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'James'
+puts person1.name
