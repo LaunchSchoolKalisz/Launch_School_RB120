ced826c100b7218b0b473a0eed749a99ded0cf24
mskalisz <mskalisz3@gmail.com>
2021-10-07 12:47:23 -0400

    Add LS solution avoid mutation

--- lesson_2/accessor_methods/8_avoid_mutation.rb
+++ lesson_2/accessor_methods/8_avoid_mutation.rb
@@ -37,4 +37,28 @@ person1.name.reverse!
 puts person1.name
 
 =begin
+LS solution
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name.clone
+  end
+end
+
+person1 = Person.new('James')
+person1.name.reverse!
+puts person1.name
+
+Discussion
+In the initial example, @name was modified through the invocation of a destructive method. This is because
+getter methods typically return a reference to the instance variable. If you use this reference to mutate the 
+return value, you also mutate the instance variable.
+
+To prevent this, we can invoke the #clone method on @name, which returns a copy of @name instead of a 
+reference to it. This means that any modifications done to the return value won't affect the value of @name. 
+Note that we should also remove the attr_reader line since we are providing a customized getter method.
 =end
