9247bbd2c4b9b34f14de30f0c0c6f5f674b870bf
mskalisz <mskalisz3@gmail.com>
2021-10-07 09:15:52 -0400

    Add LS solution method lookup 1

--- lesson_2/inheritance/oop_basics_inheritance/7_method_lookup_1.rb
+++ lesson_2/inheritance/oop_basics_inheritance/7_method_lookup_1.rb
@@ -20,4 +20,17 @@ cat1 = Cat.new('Black')
 cat1.color
 
 Cat --> Animal
+
+LS Solution
+
+Cat
+Animal
+
+Discussion
+When a method is invoked, Ruby searches the method's class for the specified method. If no method is found, 
+then Ruby inspects the class's superclass. This process is repeated until the method is found or there are no 
+more classes.
+
+To find the lookup path for a class, simply invoke #ancestors on the class. This method returns an array 
+containing the names of the classes in the lookup path, based on the order in which they're checked.
 =end
