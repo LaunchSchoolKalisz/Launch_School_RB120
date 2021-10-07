aedce968014233c47c699d07fd1dc06605ee818c
mskalisz <mskalisz3@gmail.com>
2021-10-07 09:31:50 -0400

    Solve transportation

--- lesson_2/inheritance/oop_basics_inheritance/10_transportation.rb
+++ lesson_2/inheritance/oop_basics_inheritance/10_transportation.rb
@@ -2,3 +2,16 @@
 Create a module named Transportation that contains three classes: Vehicle, Truck, and Car. 
 Truck and Car should both inherit from Vehicle.
 =end
+
+module Transportation
+end
+
+class Vehicle
+  include Transportation
+end
+
+class Truck < Vehicle
+end
+
+class Car < Vehicle
+end
