44d3620786c7dcaa955e3fba00f2a972c2d5d23f
mskalisz <mskalisz3@gmail.com>
2021-10-08 07:34:26 -0400

    Add prompt for flight data

---
+++ lesson_2/easy_1/6_flight_data.rb
@@ -0,0 +1,14 @@
+=begin
+Consider the following class definition:
+
+class Flight
+  attr_accessor :database_handle
+
+  def initialize(flight_number)
+    @database_handle = Database.init
+    @flight_number = flight_number
+  end
+end
+
+=end
+
