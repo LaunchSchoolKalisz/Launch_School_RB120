4317d935ef95dfc4c87057007b28285e2ab08130
mskalisz <mskalisz3@gmail.com>
2021-10-07 20:31:05 -0400

    Add LS solution to whats the output

--- lesson_2/easy_1/2_whats_the_output.rb
+++ lesson_2/easy_1/2_whats_the_output.rb
@@ -23,18 +23,36 @@ puts name
 
 What output does this code print? Fix this class so that there are no surprises waiting in store for the 
 unsuspecting developer.
+
+Output:
+Fluffy
+My name is FLUFFY.
+FLUFFY
+FLUFFY
 =end
 
-class Pet
+# class Pet
+#   attr_reader :name
+
+#   def initialize(name)
+#     @name = name.clone.to_s
+#   end
+
+#   def to_s
+#     @name.upcase!
+#     "My name is #{@name}."
+#   end
+# end
+
+class  Pet
   attr_reader :name
 
   def initialize(name)
-    @name = name.clone.to_s
+    @name = name.to_s
   end
 
   def to_s
-    @name.upcase!
-    "My name is #{@name}."
+    "My name is #{@name.upcase}."
   end
 end
 
@@ -44,3 +62,48 @@ puts fluffy.name
 puts fluffy
 puts fluffy.name
 puts name
+
+=begin
+Output:
+
+Fluffy
+My name is FLUFFY.
+FLUFFY
+FLUFFY
+
+Corrected Class:
+
+class  Pet
+  attr_reader :name
+
+  def initialize(name)
+    @name = name.to_s
+  end
+
+  def to_s
+    "My name is #{@name.upcase}."
+  end
+end
+
+Discussion
+The original version of #to_s uses String#upcase! which mutates its argument in place. This causes @name to be
+modified, which in turn causes name to be modified: this is because @name and name reference the same object 
+in memory.
+
+Oh, and that to_s inside the initialize method? We need that for the challenge under Further Exploration.
+
+Further Exploration
+What would happen in this case?
+
+name = 42
+fluffy = Pet.new(name)
+name += 1
+puts fluffy.name
+puts fluffy
+puts fluffy.name
+puts name
+
+This code "works" because of that mysterious to_s call in Pet#initialize. However, that doesn't explain why 
+this code produces the result it does. Can you?
+
+=end
