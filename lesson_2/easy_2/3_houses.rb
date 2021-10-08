5f99a0de495b6cb2de94c25c6724dcee59506d42
mskalisz <mskalisz3@gmail.com>
2021-10-08 09:46:04 -0400

    Add solution for houses

--- lesson_2/easy_2/3_houses.rb
+++ lesson_2/easy_2/3_houses.rb
@@ -23,3 +23,17 @@ Modify the House class so that the above program will work. You are permitted to
 one new method in House.
 
 =end
+
+class House
+  attr_reader :price
+
+  def initialize(price)
+    @price = price
+  end
+
+end
+
+home1 = House.new(100_000)
+home2 = House.new(150_000)
+puts "Home 1 is cheaper" if home1.price < home2.price
+puts "Home 2 is more expensive" if home2.price > home1.price
