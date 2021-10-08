6d83ce44b5a4e54f0ab20b4493298f2b4d2390cb
mskalisz <mskalisz3@gmail.com>
2021-10-08 10:16:52 -0400

    Solve reverse engineering

--- lesson_2/easy_2/4_reverse_engineering.rb
+++ lesson_2/easy_2/4_reverse_engineering.rb
@@ -10,3 +10,26 @@ my_data = Transform.new('abc')
 puts my_data.uppercase
 puts Transform.lowercase('XYZ')
 =end
+
+module Caseable
+end
+
+class Transform
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def self.lowercase(words = @words)
+    words.downcase
+  end
+
+  def uppercase(words = @words)
+    words.upcase
+  end
+end
+
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')
