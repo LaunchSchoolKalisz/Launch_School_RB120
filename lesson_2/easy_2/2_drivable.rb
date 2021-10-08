0fe0c171609843cd274c53b4feea99b302f13a9d
mskalisz <mskalisz3@gmail.com>
2021-10-08 09:34:24 -0400

    Add LS solution to drivable

--- lesson_2/easy_2/2_drivable.rb
+++ lesson_2/easy_2/2_drivable.rb
@@ -27,3 +27,18 @@ end
 
 bobs_car = Car.new
 bobs_car.drive
+
+=begin
+LS Solution
+
+Modify the self.drive method in Drivable to simply drive:
+
+def drive
+end
+
+Discussion
+Methods in mixin modules should be defined without using self. in the definition. If you add 
+self. to the definition, the including class will only be able to access the method by calling 
+Drivable.drive; furthermore, the method will not be available at all as an instance method to 
+objects.
+=end
