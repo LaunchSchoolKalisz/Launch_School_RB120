69cfca96ab4cd1baab75338f70d444a7dbd58b70
mskalisz <mskalisz3@gmail.com>
2021-10-08 09:48:13 -0400

    Add LS solution to houses

--- lesson_2/easy_2/3_houses.rb
+++ lesson_2/easy_2/3_houses.rb
@@ -37,3 +37,37 @@ home1 = House.new(100_000)
 home2 = House.new(150_000)
 puts "Home 1 is cheaper" if home1.price < home2.price
 puts "Home 2 is more expensive" if home2.price > home1.price
+
+=begin
+LS Solution
+
+class House
+  attr_reader :price
+  include Comparable
+
+  def initialize(price)
+    @price = price
+  end
+
+  def <=>(other)
+    price <=> other.price
+  end
+end
+
+...
+
+Discussion
+Making objects comparable is actually quite easy; you don't have to create every possible 
+comparison operator for the object. Instead, all you need to do is include the Comparable mixin, 
+and define one method: <=>. The <=> method should return 0 if the two objects are "equal", 1 if 
+the receiving object is greater than the other object, and -1 if the receiving object is less 
+than the other object. Often, as here, the comparison will boil down to comparing numbers or 
+strings, both of which already have a <=> operator defined. Thus, you rarely have to write an 
+involved #<=> method.
+
+Further Exploration
+Is the technique we employ here to make House objects comparable a good one? (Hint: is there a 
+natural way to compare Houses? Is price the only criteria you might use?) What problems might 
+you run into, if any? Can you think of any sort of classes where including Comparable is a 
+good idea?
+=end
