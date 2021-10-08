66d3e20be6829ed188686f0777fa1bd1a2df958c
mskalisz <mskalisz3@gmail.com>
2021-10-08 07:36:08 -0400

    Add LS soluttion to flight data

--- lesson_2/easy_1/6_flight_data.rb
+++ lesson_2/easy_1/6_flight_data.rb
@@ -15,5 +15,22 @@ How can this class be fixed to be resistant to future problems?
 
 Right now anyoone can set the database handle, which may lead to mistakes down the road. We can separate 
 the gettter and setter methods and then make the setter method private or protected
+
+LS Solution
+Delete the following line:
+
+attr_accessor :database_handle
+
+Discussion
+The problem with this definition is that we are providing easy access to the @database_handle instance 
+variable, which is almost certainly just an implementation detail. As an implementation detail, users of this 
+class should have no need for it, so we should not be providing direct access to it.
+
+The fix is easy: don't provide the unneeded and unwanted attr_accessor.
+
+What can go wrong if we don't change things? First off, by making access to @database_handle easy, someone may 
+use it in real code. And once that database handle is being used in real code, future modifications to the 
+class may break that code. You may even be prevented from modifying your class at all if the dependent code is 
+of greater concern.
 =end
 
