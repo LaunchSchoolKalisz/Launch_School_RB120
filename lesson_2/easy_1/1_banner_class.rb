3dfb401b55a76304585144fdacd4140af910dfd2
mskalisz <mskalisz3@gmail.com>
2021-10-07 20:05:48 -0400

    Solve banner class

--- lesson_2/easy_1/1_banner_class.rb
+++ lesson_2/easy_1/1_banner_class.rb
@@ -47,3 +47,54 @@ puts banner
 +--+
 
 =end
+
+class Banner
+  def initialize(message)
+    @message = message
+  end
+
+  def to_s
+    [horizontal_rule, empty_line, message_line, empty_line, horizontal_rule].join("\n")
+  end
+
+  private
+
+  def horizontal_rule
+    lines = "-" * message_size
+    "+-#{lines}-+"
+  end
+
+  def empty_line
+    spaces = " " * message_size
+    "| #{spaces} |"
+  end
+
+  def message_line
+    "| #{@message} |"
+  end
+
+  def message_size
+    @message.size
+  end
+
+end
+
+banner = Banner.new('To boldly go where no one has gone before.')
+puts banner
+=begin
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+=end
+
+banner = Banner.new('')
+puts banner
+=begin
++--+
+|  |
+|  |
+|  |
++--+
+=end
