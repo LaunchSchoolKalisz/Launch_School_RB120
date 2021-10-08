f743484a868d5b9cac474d212028b499475e6eae
mskalisz <mskalisz3@gmail.com>
2021-10-08 10:07:10 -0400

    Add prompt for reverse engineering

---
+++ lesson_2/easy_2/4_reverse_engineering.rb
@@ -0,0 +1,12 @@
+=begin
+Write a class that will display:
+
+ABC
+xyz
+
+when the following code is run:
+
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')
+=end
