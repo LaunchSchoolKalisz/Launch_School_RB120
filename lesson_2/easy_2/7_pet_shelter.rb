527a870346c7c4cb90edf1e9626074e7e6072fdf
mskalisz <mskalisz3@gmail.com>
2021-10-09 11:09:43 -0400

    Develop pet, owner, shelter classes with general structure. Need to format print adoptions method

--- lesson_2/easy_2/7_pet_shelter.rb
+++ lesson_2/easy_2/7_pet_shelter.rb
@@ -41,4 +41,66 @@ P Hanson has 3 adopted pets.
 B Holmes has 4 adopted pets.
 
 The order of the output does not matter, so long as all of the information is presented.
+
+# Create Pet, Owner, and Shelter classes
 =end
+
+class Pet
+  attr_accessor :animal_type, :name
+
+  def initialize(animal_type, name)
+    @animal_type = animal_type
+    @name = name
+  end
+end
+
+class Owner
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Shelter
+  attr_accessor :adoption_list
+
+  def initialize
+    @adoption_list = {}
+  end
+
+  def adopt(owner, pet)
+    if @adoption_list.include?(owner.name)
+      self.adoption_list[owner.name] << pet
+    else
+      self.adoption_list[owner.name] = [pet]
+    end
+  end
+
+  def print_adoptions
+    puts @adoption_list
+  end
+end
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+# puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+# puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
