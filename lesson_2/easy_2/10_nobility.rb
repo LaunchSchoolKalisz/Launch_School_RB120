5ee7e6a70ea1e871e18910e824615a4fd7ee3170
mskalisz <mskalisz3@gmail.com>
2021-10-11 21:25:57 -0400

    Solve nobility

--- lesson_2/easy_2/10_nobility.rb
+++ lesson_2/easy_2/10_nobility.rb
@@ -16,3 +16,90 @@ byron.name
 byron.title
 => "Lord"
 =end
+
+module Walkable
+  def walk
+    puts "#{self.name} #{self.gait} forward"
+  end
+end
+
+
+class Person
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "strolls"
+  end
+end
+
+class Cat
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "saunters"
+  end
+end
+
+class Cheetah
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "runs"
+  end
+end
+
+class Noble
+  include Walkable
+  attr_reader :name, :title
+
+  def initialize(name, title)
+    @first_name = name
+    @title = title
+    @name = @title + ' ' + @first_name
+  end
+
+  private
+
+  def gait
+    "struts"
+  end
+end
+
+byron = Noble.new("Byron", "Lord")
+byron.walk
+# => "Lord Byron struts forward"
+p byron.name
+p byron.title
+
+mike = Person.new("Mike")
+mike.walk
+# => "Mike strolls forward"
+
+kitty = Cat.new("Kitty")
+kitty.walk
+# => "Kitty saunters forward"
+
+flash = Cheetah.new("Flash")
+flash.walk
+# => "Flash runs forward"
