1543b327ac72094c7debb43dbe9724d230e8cfde
mskalisz <mskalisz3@gmail.com>
2021-10-15 08:34:54 -0400

    Fix all rubocop offenses

--- lesson_2/walkthrough_oo_rps.rb
+++ lesson_2/walkthrough_oo_rps.rb
@@ -16,10 +16,12 @@ class RPSGame
     puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}"
     puts "The computer, #{computer.name}, chose #{computer.move}"
+  end
 
+  def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -47,6 +49,7 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
