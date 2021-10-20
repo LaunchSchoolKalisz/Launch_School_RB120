6bc3befc0f5e4ef82617cdd21d96ebe1963c729b
mskalisz <mskalisz3@gmail.com>
2021-10-20 06:28:44 -0400

    Solve exercise 2

--- lesson_4/easy_1/2.rb
+++ lesson_4/easy_1/2.rb
@@ -22,3 +22,28 @@ class Truck
 end
 
 =end
+
+module Speed
+  def go_fast
+    puts "I am a #{self.class} and going super fast!"
+  end
+end
+
+class Car
+  include Speed
+
+  def go_slow
+    puts "I am safe and driving slow."
+  end
+end
+
+class Truck
+  include Speed
+
+  def go_very_slow
+    puts "I am a heavy truck and like going very slow."
+  end
+end
+
+p Car.new.go_fast
+p Truck.new.go_fast
