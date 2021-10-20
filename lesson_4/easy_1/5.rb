027c2f125fbb57c10ff6ad14bc268bcdc7dc0652
mskalisz <mskalisz3@gmail.com>
2021-10-20 07:21:44 -0400

    Add LS solution for exercise 5

--- lesson_4/easy_1/5.rb
+++ lesson_4/easy_1/5.rb
@@ -14,4 +14,31 @@ class Pizza
 end
 
 Pizza has an instance variable - defined by @name
+
+LS Solution
+
+You can find out if an object has instance variables by either looking at the class or asking the 
+object. First, lets look at the class definitions.
+
+You might have noticed in the Pizza class there is a variable where the variable name starts with 
+an @ symbol. This means that this class has an instance variable.
+
+But let us be triple sure that only Pizza has an instance variable by asking our objects if they 
+have instance variables.
+
+To do this we first need to create a Pizza object and a Fruit object.
+
+hot_pizza = Pizza.new("cheese")
+orange    = Fruit.new("apple")
+Now we can ask each of these objects if they have instance variables.
+
+>> hot_pizza.instance_variables
+=> [:@name]
+>> orange.instance_variables
+=> []
+
+As you can see, if we call the instance_variables method on the instance of the class we will be 
+informed if the object has any instance variables and what they are.
+
+By doing this we have found out that Pizza has instance variables while Fruit does not.
 =end
