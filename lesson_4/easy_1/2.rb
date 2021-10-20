97c7b246ac3fa5d9f8e6e84ac91493b6905e8f47
mskalisz <mskalisz3@gmail.com>
2021-10-20 06:30:41 -0400

    Add LS Solution exercise 2

--- lesson_4/easy_1/2.rb
+++ lesson_4/easy_1/2.rb
@@ -47,3 +47,40 @@ end
 
 p Car.new.go_fast
 p Truck.new.go_fast
+
+=begin
+In order to give the go_fast method to the Truck and the Car we need to include the module that 
+has that method. To do this we need to add include Speed to each of the classes, like this:
+
+module Speed
+  def go_fast
+    puts "I am a #{self.class} and going super fast!"
+  end
+end
+
+class Car
+  include Speed
+  def go_slow
+    puts "I am safe and driving slow."
+  end
+end
+
+class Truck
+  include Speed
+  def go_very_slow
+    puts "I am a heavy truck and like going very slow."
+  end
+end
+Now that everything looks right, how do we confirm that our Truck and Car can actually go_fast? 
+The answer is to try it:
+
+irb :001 > blue_truck = Truck.new
+irb :002 > blue_truck.go_fast
+I am a Truck and going super fast!
+
+irb :003 > small_car = Car.new
+irb :004 > small_car.go_fast
+I am a Car and going super fast!
+
+As you can see we can now go fast in our Car and Truck.
+=end
