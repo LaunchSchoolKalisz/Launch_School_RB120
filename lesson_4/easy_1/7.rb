e0d330190a5c105718d4f48b091c37a7885b9ff6
mskalisz <mskalisz3@gmail.com>
2021-10-20 07:32:21 -0400

    Add LS solution for exercise 7

--- lesson_4/easy_1/7.rb
+++ lesson_4/easy_1/7.rb
@@ -18,3 +18,14 @@ end
 
 big_cube = Cube.new(5000)
 p big_cube.to_s
+
+=begin
+LS Solution
+
+By default, the to_s method will return the name of the object's class and an encoding of the 
+object id.
+
+If you weren't sure of this answer you could of course refer to Launch School's Object Oriented 
+Programming book, but you could also refer directly to the Ruby documentation, in this case, 
+here: http://ruby-doc.org/core/Object.html#method-i-to_s.
+=end
