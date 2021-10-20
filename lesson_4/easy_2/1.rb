0d78c0365eef3645df606c33631c27ae48de7f0a
mskalisz <mskalisz3@gmail.com>
2021-10-20 14:36:48 -0400

    Add prompt for exercise 1

---
+++ lesson_4/easy_2/1.rb
@@ -0,0 +1,19 @@
+=begin
+You are given the following code:
+
+class Oracle
+  def predict_the_future
+    "You will " + choices.sample
+  end
+
+  def choices
+    ["eat a nice lunch", "take a nap soon", "stay at work late"]
+  end
+end
+
+What is the result of executing the following code:
+
+oracle = Oracle.new
+oracle.predict_the_future
+
+=end
