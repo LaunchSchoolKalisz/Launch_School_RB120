effd2d8e52d5f5724b0f21019ad2fdb4ed5ef91b
mskalisz <mskalisz3@gmail.com>
2021-10-20 07:30:56 -0400

    Add solution for exercise 7

--- lesson_4/easy_1/7.rb
+++ lesson_4/easy_1/7.rb
@@ -2,4 +2,19 @@
 What is the default return value of to_s when invoked on an object? Where could you go to find out 
 if you want to be sure?
 
+to_s returns the object ID: "#<Cube:0x00007fc8ca11ff18>"
+
 =end
+
+class Cube
+  def initialize(volume)
+    @volume = volume
+  end
+
+  def get_volume
+    @volume
+  end
+end
+
+big_cube = Cube.new(5000)
+p big_cube.to_s
