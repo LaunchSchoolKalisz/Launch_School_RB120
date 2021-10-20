eae8b4c58b9b6883e52c61242c6f1262526a3055
mskalisz <mskalisz3@gmail.com>
2021-10-20 06:21:55 -0400

    Add LS solution for exercise 1

--- lesson_4/easy_1/1.rb
+++ lesson_4/easy_1/1.rb
@@ -14,3 +14,23 @@ p true.class
 p "hello".class
 p [1, 2, 3, "happy days"].class
 p 142.class
+
+=begin
+LS Solution
+
+All of them are objects! Everything in Ruby is an object, so you never need to ask yourself "is 
+this an object?" because the answer every time will be yes.
+
+You can find out what class an object belongs to in Ruby by asking the object what its class is 
+by calling the method class on the object, as you can see below:
+
+irb :001 > true.class
+=> TrueClass
+irb :002 > "hello".class
+=> String
+irb :003 > [1, 2, 3, "happy days"].class
+=> Array
+irb :004 > 142.class
+=> Integer
+
+=end
