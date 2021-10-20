16688bd88f88ff50d0371fde40a4898c38b3203b
mskalisz <mskalisz3@gmail.com>
2021-10-20 06:20:18 -0400

    Solve easy_1 exercise 1

---
+++ lesson_4/easy_1/1.rb
@@ -0,0 +1,16 @@
+=begin
+Which of the following are objects in Ruby? If they are objects, how can you find out what class 
+they belong to?
+
+true
+"hello"
+[1, 2, 3, "happy days"]
+142
+
+They are all objects. You can find the class by:
+=end
+
+p true.class
+p "hello".class
+p [1, 2, 3, "happy days"].class
+p 142.class
