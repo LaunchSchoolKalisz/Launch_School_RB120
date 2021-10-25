3bea97c835ea0043dbe63c95625a2e49d12ebbb4
mskalisz <mskalisz3@gmail.com>
2021-10-24 22:02:13 -0400

    Add LS solution for exercise 9

--- lesson_4/easy_2/9.rb
+++ lesson_4/easy_2/9.rb
@@ -17,4 +17,27 @@ What would happen if we added a play method to the Bingo class, keeping in mind 
 already a method of this name in the Game class that the Bingo class inherits from.
 
 The play method in the Bingo class would override the play method in the game class
+
+LS Solution
+
+If we added a new method to the Bingo class as seen below, it will use that method instead of 
+looking up the chain and finding the Game class's method. Because Ruby doesn't want to look all 
+over the place, as soon as it finds a method that matches it uses that - so in this case it is 
+really first come first served.
+
+class Game
+  def play
+    "Start the game!"
+  end
+end
+
+class Bingo < Game
+  def rules_of_play
+    #rules of play
+  end
+
+  def play
+    "Eyes down"
+  end
+end
 =end
