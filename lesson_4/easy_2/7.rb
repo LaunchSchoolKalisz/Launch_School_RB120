e967f5e0f8831d043350af81491061f47863e42c
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:50:48 -0400

    Solve exercise 7

--- lesson_4/easy_2/7.rb
+++ lesson_4/easy_2/7.rb
@@ -17,4 +17,26 @@ end
 
 Explain what the @@cats_count variable does and how it works. What code would you need to write to 
 test your theory?
+
+Cats count increments by 1 when a new Cat is initialized
+
+To test:
 =end
+
+class Cat
+  @@cats_count = 0
+
+  def initialize(type)
+    @type = type
+    @age  = 0
+    @@cats_count += 1
+  end
+
+  def self.cats_count
+    @@cats_count
+  end
+end
+
+p Cat.cats_count
+tabby_cat = Cat.new('tabby')
+p Cat.cats_count
