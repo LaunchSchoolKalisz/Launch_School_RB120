896781df9355d0b0f96012e24fa3e17898733eb9
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:37:44 -0400

    Add LS solution for exercise 6

--- lesson_4/easy_2/6.rb
+++ lesson_4/easy_2/6.rb
@@ -14,4 +14,11 @@ end
 Which one of these is a class method (if any) and how do you know? How would you call a class 
 method?
 manufacturer is a class method because self is called with the method
+
+LS Solution
+Class methods in Ruby start with self so in this case the self.manufacturer method is the class 
+method.
+
+You can call a class method by using the class name and then calling the method. For example here 
+it would be Television.manufacturer.
 =end
