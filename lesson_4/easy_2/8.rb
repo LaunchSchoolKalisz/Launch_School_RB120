f041a5a1545fad9ca9ca867832d7caa7c5fb3d54
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:58:06 -0400

    Add LS solution for exercise 8

--- lesson_4/easy_2/8.rb
+++ lesson_4/easy_2/8.rb
@@ -22,4 +22,28 @@ class Bingo < Game
     #rules of play
   end
 end
+
+LS solution
+To tell Ruby that the Bingo class will inherit from the Game class we need to put it after the 
+class name when defining the Bingo class. An example of this would look like:
+
+class Game
+  def play
+    "Start the game!"
+  end
+end
+
+class Bingo < Game
+  def rules_of_play
+    #rules of play
+  end
+end
+
+To test this code out we will need to create a new instance of the class Bingo and then call the 
+play method on that instance, as you can see below:
+
+>> game_of_bingo = Bingo.new
+=> #<Bingo:0x007f9d19b537c8>
+>> game_of_bingo.play
+=> "Start the game!"
 =end
