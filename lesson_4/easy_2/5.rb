96adfb3fa71f5ee25e28a80bc33eea725d89c942
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:34:40 -0400

    Add LS solution for exercise 5

--- lesson_4/easy_2/5.rb
+++ lesson_4/easy_2/5.rb
@@ -8,5 +8,12 @@ excited_dog = "excited dog" - method variable
 
 they are all named differently eith @'s
 
+LS Solution
+Here we have the following variables:
 
+Local variable - excited_dog Instance variable - @excited_dog Class variable - @@excited_dog
+
+We can tell which is which by how the variables are prefixed. Local variables do not contain 
+anything prefixed, while instance variables are prefixed with the @ and class variables are 
+prefixed with @@.
 =end
