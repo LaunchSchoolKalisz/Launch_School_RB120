a441048cc9f6fb70dfdec8c6942830130f281089
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:19:21 -0400

    Add prompt exercise 5

---
+++ lesson_4/easy_2/5.rb
@@ -0,0 +1,8 @@
+=begin
+There are a number of variables listed below. What are the different types and how do you know 
+which is which?
+
+excited_dog = "excited dog"
+@excited_dog = "excited dog"
+@@excited_dog = "excited dog"
+=end
