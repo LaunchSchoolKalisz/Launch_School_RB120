329f15873ef850c589628b6d1e0c1c83f97916a0
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:18:35 -0400

    Add LS solution exercise 4

--- lesson_4/easy_2/4.rb
+++ lesson_4/easy_2/4.rb
@@ -22,4 +22,36 @@ end
 
 attr_accessor @type
 
+LS Solution
+
+We can add attr_accessor to the top of the class. And it will give us the ability to get and set 
+the @type instance variable the same as we can do now.
+
+This simplifies the class substantially and now the class would look like:
+
+class BeesWax
+  attr_accessor :type
+
+  def initialize(type)
+    @type = type
+  end
+
+  def describe_type
+    puts "I am a #{@type} type of Bees Wax"
+  end
+end
+
+While this is much better, there is still something we can improve.
+
+Currently, inside the describe_type method, we are referencing the @type variable with the @ 
+symbol, but this is not needed. As there is a method in the class which replaces the need to 
+access the instance variable directly we can change the describe_type method to be:
+
+def describe_type
+  puts "I am a #{type} type of Bees Wax"
+end
+
+This is much cleaner, and it is standard practice to refer to instance variables inside the 
+class without @ if the getter method is available.
+
 =end
