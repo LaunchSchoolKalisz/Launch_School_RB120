4e2b6575ebd9b2a2b07be49e32bc5dfaa1f07871
mskalisz <mskalisz3@gmail.com>
2021-10-24 22:01:28 -0400

    Add solution for exercise 9

--- lesson_4/easy_2/9.rb
+++ lesson_4/easy_2/9.rb
@@ -15,4 +15,6 @@ end
 
 What would happen if we added a play method to the Bingo class, keeping in mind that there is 
 already a method of this name in the Game class that the Bingo class inherits from.
+
+The play method in the Bingo class would override the play method in the game class
 =end
