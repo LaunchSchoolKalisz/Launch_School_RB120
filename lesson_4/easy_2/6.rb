1d792b70bffabaa214c8e6336c6cd2df1a6fa434
mskalisz <mskalisz3@gmail.com>
2021-10-24 21:35:39 -0400

    Add prompt for exercise 6

---
+++ lesson_4/easy_2/6.rb
@@ -0,0 +1,16 @@
+=begin
+If I have the following class:
+
+class Television
+  def self.manufacturer
+    # method logic
+  end
+
+  def model
+    # method logic
+  end
+end
+
+Which one of these is a class method (if any) and how do you know? How would you call a class 
+method?
+=end
