668d2cdcfc738754bbb39817ca509510900f34a4
mskalisz <mskalisz3@gmail.com>
2021-10-28 10:46:58 -0400

    Add LS solution for exercise 2

--- lesson_4/easy_3/2.rb
+++ lesson_4/easy_3/2.rb
@@ -41,3 +41,20 @@ class Goodbye < Greeting
 end
 
 Hello.new.hi
+
+=begin
+LS Solution
+
+You could define the hi method on the Hello class as follows:
+
+class Hello
+  def self.hi
+    greeting = Greeting.new
+    greeting.greet("Hello")
+  end
+end
+
+This is rather cumbersome. Note that we cannot simply call greet in the self.hi method definition 
+because the Greeting class itself only defines greet on its instances, rather than on the Greeting class 
+itself.
+=end
