e945921de31bb6033420fc75b5ddba64ee521d79
mskalisz <mskalisz3@gmail.com>
2021-10-28 10:45:21 -0400

    Solve exercise 2

--- lesson_4/easy_3/2.rb
+++ lesson_4/easy_3/2.rb
@@ -21,3 +21,23 @@ end
 
 If we call Hello.hi we get an error message. How would you fix this?
 =end
+
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+Hello.new.hi
