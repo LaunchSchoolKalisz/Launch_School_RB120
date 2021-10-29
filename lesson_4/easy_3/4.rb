3d5ca9a9ba9af5516436828bf5d35a49ed81c85d
mskalisz <mskalisz3@gmail.com>
2021-10-28 22:48:20 -0400

    Add LS solution for exercise 4

--- lesson_4/easy_3/4.rb
+++ lesson_4/easy_3/4.rb
@@ -24,3 +24,40 @@ end
 
 cat = Cat.new("tabby")
 cat.to_s
+
+=begin
+LS Solution
+To do this we would need to override the existing to_s method by adding a method of the same name as 
+this to the class. It would look something like this:
+
+class Cat
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+
+  def to_s
+    "I am a #{type} cat"
+  end
+end
+
+Note that we have added a getter method which allows us to retrieve the type instance variable.
+
+We can customize existing methods like this easily, but in many cases it might be better to write a new 
+method called something like display_type instead, as this is more specific about what we are actually 
+wanting the result of the method to be. An example of this would be:
+
+class Cat
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+
+  def display_type
+    puts "I am a #{type} cat"
+  end
+end
+
+=end
