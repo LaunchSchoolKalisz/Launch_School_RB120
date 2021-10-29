d4e09153767e8bf6f1048538ecb113a285911f6a
mskalisz <mskalisz3@gmail.com>
2021-10-28 22:46:38 -0400

    Solve exercise 4

--- lesson_4/easy_3/4.rb
+++ lesson_4/easy_3/4.rb
@@ -11,3 +11,16 @@ end
 How could we go about changing the to_s output on this method to look like this: I am a tabby cat? 
 (this is assuming that "tabby" is the type we passed in during initialization).
 =end
+
+class Cat
+  def initialize(type)
+    @type = type
+  end
+
+  def to_s
+    puts "I am a #{@type} cat"
+  end
+end
+
+cat = Cat.new("tabby")
+cat.to_s
