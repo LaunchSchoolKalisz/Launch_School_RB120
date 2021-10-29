fed48b0c9c27363c950c6d4aa847a760118cf2b2
mskalisz <mskalisz3@gmail.com>
2021-10-28 23:03:25 -0400

    Add LS solution to exercise 6

--- lesson_4/easy_3/6.rb
+++ lesson_4/easy_3/6.rb
@@ -20,4 +20,24 @@ we don't have to use the self prefix?
   def make_one_year_older
     @age += 1
   end
+
+LS Solution
+
+self in this case is referencing the setter method provided by attr_accessor - this means that we could 
+replace self with @. So the revised method would look something like this:
+
+class Cat
+  attr_accessor :type, :age
+
+  def initialize(type)
+    @type = type
+    @age  = 0
+  end
+
+  def make_one_year_older
+    @age += 1
+  end
+end
+
+This means in this case self and @ are the same thing and can be used interchangeably.
 =end
