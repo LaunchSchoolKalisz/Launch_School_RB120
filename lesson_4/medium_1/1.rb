36db08143c6677b74f0d570d3389ae411ff73b0f
mskalisz <mskalisz3@gmail.com>
2021-10-29 11:37:40 -0400

    Add LS solution for exercise 1

--- lesson_4/medium_1/1.rb
+++ lesson_4/medium_1/1.rb
@@ -39,3 +39,13 @@ end
 
 acct = BankAccount.new(1000)
 p acct.positive_balance?
+
+=begin
+LS Solution
+
+Ben is right because of the fact that he added an attr_reader for the balance instance variable. This 
+means that Ruby will automatically create a method called balance that returns the value of the @balance 
+instance variable. The body of the positive_balance? method will evaluate to calling the balance method 
+of the class, which will return the value of the @balance instance variable. If Ben had omitted the 
+attr_reader (or had used an attr_writer rather than a reader or accessor) then Alyssa would be right.
+=end
