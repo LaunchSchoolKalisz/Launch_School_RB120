85b88ff6d12b3a2be31a98db6ba7557e7472745b
mskalisz <mskalisz3@gmail.com>
2021-11-03 06:38:34 -0400

    Improve light_status name for clarity for exercise 7

--- lesson_4/medium_1/7.rb
+++ lesson_4/medium_1/7.rb
@@ -16,3 +16,17 @@ class Light
 end
 
 =end
+
+class Light
+  attr_accessor :brightness, :color
+
+  def initialize(brightness, color)
+    @brightness = brightness
+    @color = color
+  end
+
+  def status
+    "I have a brightness level of #{brightness} and a color of #{color}"
+  end
+
+end
