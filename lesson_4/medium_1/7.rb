f7dbbd8319dcb3add73274f1c6641d60cc7fad19
mskalisz <mskalisz3@gmail.com>
2021-11-03 06:39:26 -0400

    Add LS solution for exercise 7

--- lesson_4/medium_1/7.rb
+++ lesson_4/medium_1/7.rb
@@ -30,3 +30,13 @@ class Light
   end
 
 end
+
+=begin
+LS Solution
+
+Currently the method is defined as light_status, which seems reasonable. But when using or invoking the 
+method, we would call it like this: my_light.light_status. Having the word "light" appear twice is 
+redundant. Therefore, we can rename the method to just status, and we can invoke it like as 
+my_light.status. This reads much better -- remember, you're writing code to be readable by others as 
+well as your future self.
+=end
