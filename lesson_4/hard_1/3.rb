3b8040e0dd451cc661870e0da13d61cf080e005a
mskalisz <mskalisz3@gmail.com>
2021-11-06 11:32:30 -0400

    Add previous code from LS solution

--- lesson_4/hard_1/3.rb
+++ lesson_4/hard_1/3.rb
@@ -3,3 +3,43 @@ Building on the prior vehicles question, we now must also track a basic motorboa
 and hull, but otherwise behaves similar to a catamaran. Therefore, creators of Motorboat instances don't need to specify 
 number of hulls or propellers. How would you modify the vehicles code to incorporate a new Motorboat class?
 =end
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Catamaran
+  include Moveable
+
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+
+    # ... other code to track catamaran-specific data omitted ...
+  end
+end
