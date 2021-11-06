df0bd7d09caa119ae1eb3cd1b96343ff936553af
mskalisz <mskalisz3@gmail.com>
2021-11-06 11:43:12 -0400

    Edit motorboat class to set up with 1 hull and 1 propeller

--- lesson_4/hard_1/3.rb
+++ lesson_4/hard_1/3.rb
@@ -48,6 +48,10 @@ class Catamaran < Seacraft
 end
  
 class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set up 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
 end
 
 =begin
