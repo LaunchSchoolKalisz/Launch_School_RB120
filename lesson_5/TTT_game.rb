72446745b78d4a94f5a4eeaa1f8c2494ff85b06e
mskalisz <mskalisz3@gmail.com>
2021-11-06 12:01:37 -0400

    Add description for tic tac toe game

---
+++ lesson_5/TTT_game.rb
@@ -0,0 +1,6 @@
+=begin 
+GAME DESCRIPTION
+
+Tic tac toe is a 2 player game. The game is set up with a 9-square grid. Player 1 marks the board with an x. Player 2 counters
+with their marker, an o. The players take turns, tryng to mark 3 consecutive squares on the grid with their markers (winner).
+=end
