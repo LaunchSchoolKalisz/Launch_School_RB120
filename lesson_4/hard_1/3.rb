e3f2f7eefc77df9dd08ea3f71f6ba02c612e302e
mskalisz <mskalisz3@gmail.com>
2021-11-06 11:38:42 -0400

    Add LS solution to exercise 3

--- lesson_4/hard_1/3.rb
+++ lesson_4/hard_1/3.rb
@@ -55,3 +55,55 @@ class Motorboat
     # ... other code to track motorboat-specific data omitted ...
   end
 end
+
+=begin
+We can create a new class to present the common elements of motorboats and catamarans. We can call it, for example, Seacraft. We 
+still want to include the Moveable module to get the support for calculating the range.
+
+class Seacraft
+  include Moveable
+
+  attr_reader :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    self.fuel_efficiency = fuel_efficiency
+    self.fuel_capacity = fuel_capacity
+  end
+end
+
+We can now implement Motorboat based on the Seacraft definition. We don't need to include a reference to Moveable since that is 
+already included in the Seacraft super class.
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set up 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+And we alter the Catamaran to inherit from Seacraft and move hull and propeller tracking out since it's taken over by Seacraft. 
+We can also remove the reference to the Moveable module.
+
+class Catamaran < Seacraft
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    super(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+The super method automatically receives and passes along any arguments which the original method received. Because of that, we can remove 
+the arguments being passed into super:
+
+class Catamaran < Seacraft
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    super
+  end
+end
+
+In fact, because super is the only statement in this initialize method and there's nothing to override, we can remove it altogether.
+
+class Catamaran < Seacraft
+end
+
+=end
