6bd4ff524ed3233dc19ea8e07b0362970bdda3a6
mskalisz <mskalisz3@gmail.com>
2021-11-13 10:36:36 -0500

    Edit board formatting for improved readability

--- lesson_5/TTT_game.rb
+++ lesson_5/TTT_game.rb
@@ -22,8 +22,8 @@ SPIKE
 require 'pry' 
 
 class TTTGame
-  HUMAN_MARKER = "X"
-  COMPUTER_MARKER = "O"
+  HUMAN_MARKER = " X "
+  COMPUTER_MARKER = " O "
 
   attr_reader :board, :human, :computer
 
@@ -46,17 +46,17 @@ class TTTGame
     system 'clear'
     puts "You are a #{human.marker}. Computer is a #{computer.marker}"
     puts ""
-    puts "     |     |"
+    puts "       |       |"
     puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts "       |       |"
+    puts "-------+-------+-------"
+    puts "       |       |"
     puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts "       |       |"
+    puts "-------+-------+-------"
+    puts "       |       |"
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
-    puts "     |     |"
+    puts "       |       |"
     puts ""
   end
 
@@ -102,7 +102,7 @@ end
 class Board
   def initialize
     @squares = {}
-    (1..9).each {|key| @squares[key] = Square.new(key.to_s)}
+    (1..9).each {|key| @squares[key] = Square.new("[#{key.to_s}]")}
   end
 
   def get_square_at(key)  
