8bd669077b0eb24139ba29899d022849ae54fa08
mskalisz <mskalisz3@gmail.com>
2021-11-27 14:33:26 -0500

    Create discription and overview of the game

---
+++ lesson_5/twenty_one.rb
@@ -0,0 +1,16 @@
+=begin
+21 Game
+
+Discription: 21 is a card game. Consists of a dealer and a player. 
+Each player's goal is to reach as close to 21 as possible without going over.
+
+Overview
+- Start with a 52-card deck
+- Each player gets 2 cards 
+- The player goes first: can hit or stay
+- If the player busts, they lose the game. If they stay, it is the dealer's turn
+- The dealer must hit until his cards add up to at least 17.
+- If dealer busts, the player wins. 
+- If both player and dealer stays, then the highest total wins.
+- If both totals are equal, then it's a tie, and nobody wins.
+=end
