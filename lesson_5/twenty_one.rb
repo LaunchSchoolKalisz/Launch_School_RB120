fb4d7dc1c3e3b439705404e83052b0aba73c603e
mskalisz <mskalisz3@gmail.com>
2021-11-27 15:23:33 -0500

    Hash out card class

--- lesson_5/twenty_one.rb
+++ lesson_5/twenty_one.rb
@@ -94,9 +94,53 @@ class Deck
 end
 
 class Card
-  def initialize
-    # what are the "states" of a card?
+  SUITS = ['H', 'D', 'S', 'C']
+  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A']
+
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
   end
+
+  def to_s
+    "The #{suit} of #{face}}"
+  end
+
+  def face
+    case face
+    when 'J' then "Jack"
+    when 'Q' then "Queen"
+    when 'K' then "King"
+    when 'A' then "Ace"
+    else
+      @face
+    end
+  end
+
+  def suit
+    case @suit
+    when 'H' then 'Hearts'
+    when 'D' then 'Diamonds'
+    when 'S' then 'Spades'
+    when 'C' then 'Clubs'
+    end
+  end
+
+  def ace?
+    face == 'Ace'
+  end
+
+  def king?
+    face == 'King'
+  end
+
+  def queen?
+    face == 'Queen'
+  end
+
+  def jack?
+    face == 'Jack'
+  ends
 end
 
 class Game
