80836e30a0f19a389c1920fda4f98827c4fc223d
mskalisz <mskalisz3@gmail.com>
2021-11-27 15:07:58 -0500

    Spike general outline of code

--- lesson_5/twenty_one.rb
+++ lesson_5/twenty_one.rb
@@ -41,3 +41,63 @@ Card
 Game
 - Start
 =end
+
+class Player
+  def initialize
+    # what would the "data" or "states" of a Player object entail?
+    # maybe cards? a name?
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total 
+    # need info about cards
+  end
+end
+
+class Dealer
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total 
+    # need info about cards
+  end
+end
+
+class Deck
+  def initialize
+    # obviously, we need some data structure to keep track of cards
+    # array, hash, something else?
+  end
+
+  def deal
+    # does the dealer or the deck deal?
+  end
+end
+
+class Card
+  def initialize
+    # what are the "states" of a card?
+  end
+end
+
+class Game
+  def start
+    # what's the sequence of steps to execute the game play?
+  end
+end
+
+Game.new.starts
