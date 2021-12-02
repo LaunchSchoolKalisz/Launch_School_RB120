2194c9996a536e60ae4feaed9500bd4ea8272dac
mskalisz <mskalisz3@gmail.com>
2021-12-02 09:31:38 -0500

    Add LS solution for animal kingdom

--- lesson_5/debugging/2.rb
+++ lesson_5/debugging/2.rb
@@ -56,3 +56,38 @@ end
 unicornfish = Fish.new(:herbivore, 'breathe underwater')
 penguin = FlightlessBird.new(:carnivore, 'drink sea water')
 robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+
+=begin
+Solution
+
+# code omitted
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# code omitted
+
+Discussion
+On line 37 of our code (line 5 of the above solution), we must explicitly pass diet 
+and superpower to super. These specified arguments are then sent up the method lookup 
+chain to Animal#initialize. If we do not pass the two intended arguments to super on 
+line 37, then all three arguments (diet, superpower, and song) that were passed into 
+SongBird#initialize will be passed up the method lookup chain to the Animal#initialize 
+method. This raises an ArgumentError because Animal#initialize expects two arguments 
+rather than three.
+
+Review this chapter of Launch School's object oriented programming book if you need a 
+refresher on super.
+
+Further Exploration
+Is the FlightlessBird#initialize method necessary? Why or why not?
+
+=end
