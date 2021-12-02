0748da0d3b24ab678a0105912e500a804caf1a59
mskalisz <mskalisz3@gmail.com>
2021-12-02 09:21:14 -0500

    Solve exercise 1

--- lesson_5/debugging/1.rb
+++ lesson_5/debugging/1.rb
@@ -1,5 +1,6 @@
 =begin
 Community Library
+
 On line 42 of our code, we intend to display information regarding the books 
 currently checked in to our community library. Instead, an exception is raised. 
 Determine what caused this error and fix the code so that the data is displayed as 
@@ -17,6 +18,10 @@ class Library
 
   def check_in(book)
     books.push(book)
+  end
+
+  def display_books
+    books.each {|book| book.display_data}
   end
 end
 
@@ -47,4 +52,4 @@ community_library.check_in(learn_to_program)
 community_library.check_in(little_women)
 community_library.check_in(wrinkle_in_time)
 
-community_library.books.display_data
+community_library.display_books
