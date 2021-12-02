13110609f75d4ec8d684e4d1d850d65e0c04ec0b
mskalisz <mskalisz3@gmail.com>
2021-12-02 09:47:48 -0500

    Add LS solution to exercise 3

--- lesson_5/debugging/3.rb
+++ lesson_5/debugging/3.rb
@@ -46,4 +46,37 @@ ada.teleport_to(-33.89, 151.277)
 puts ada.location                   # (-33.89, 151.277)
 puts grace.location                 # (-33.89, 151.277)
 puts ada.location.to_s == grace.location.to_s # expected: true
-                                    # actual: false
+                                   
+=begin
+LS Solution
+
+class GeoLocation
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def ==(other)
+    latitude == other.latitude && longitude == other.longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+end
+
+Discussion
+On line 39 of our original code, we invoke the == method to compare the equality of 
+two locations. Since our GeoLocation class does not implement ==, Ruby invokes the 
+method from the BasicObject class. BasicObject#==, however, returns true only if the 
+two objects being compared are the same object, or in other words, if they have the 
+same object id.
+
+In order to compare the equality of instances of our custom class based on the value 
+of their attributes, we need to define a == method in our custom class, which overrides 
+BasicObject#== (thanks to Ruby's method lookup path). GeoLocation#== does exactly 
+that: it compares two locations based on their latitude and longitude values.
+
+=end
