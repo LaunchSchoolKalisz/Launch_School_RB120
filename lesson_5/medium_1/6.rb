179af39577ce2d8128ce89dbb59e585a1609fa37
mskalisz <mskalisz3@gmail.com>
2021-12-19 21:55:27 -0500

    Solve Number Guesser part 1

--- lesson_5/medium_1/6.rb
+++ lesson_5/medium_1/6.rb
@@ -64,3 +64,78 @@ You have no more guesses. You lost!
 Note that a game object should start a new game with a new number to guess with each call to #play.
 
 =end
+
+class GuessingGame
+  def initialize
+    @count = 7
+    @winning_num = (1..100).to_a.sample
+  end
+
+  def play
+    loop do
+      p @winning_num
+      break if @count <= 0
+      count
+      guess = player_guess
+      loop do
+        break if validate_guess(guess)
+        guess = player_guess
+      end
+      if guess == @winning_num
+        puts "That's the number!"
+        break 
+      end
+      feedback(guess)
+      puts "\n"
+    end
+  end
+
+  def count
+    puts "You have #{@count} guesses remaining."
+    @count -= 1
+    puts "Enter a number between 1 and 100:"
+  end
+
+  def player_guess
+    guess = gets.chomp.to_i
+  end
+
+  def validate_guess(guess)
+    return true if (1..100).to_a.include?(guess)
+    puts "Invalid guess. Enter a number between 1 and 100:"
+    false
+  end
+
+  def feedback(guess)
+    if guess > @winning_num
+      puts "Your guess is too high."
+    else
+      puts "Your guess is too low."
+    end
+  end
+  
+end
+
+game = GuessingGame.new
+game.play
+
+# You have 7 guesses remaining.
+# Enter a number between 1 and 100: 104
+# Invalid guess. Enter a number between 1 and 100: 50
+# Your guess is too low.
+
+# You have 6 guesses remaining.
+# Enter a number between 1 and 100: 75
+# Your guess is too low.
+
+# You have 5 guesses remaining.
+# Enter a number between 1 and 100: 85
+# Your guess is too high.
+
+# You have 4 guesses remaining.
+# Enter a number between 1 and 100: 0
+# Invalid guess. Enter a number between 1 and 100: 80
+
+# You have 3 guesses remaining.
+# Enter a number between 1 and 100: 81
+# That's the number!
