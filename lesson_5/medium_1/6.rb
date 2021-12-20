642dad1718064e93514c2b99c08a1612321132d8
mskalisz <mskalisz3@gmail.com>
2021-12-19 21:59:43 -0500

    Add LS solution for Number Guesser Part 1

--- lesson_5/medium_1/6.rb
+++ lesson_5/medium_1/6.rb
@@ -73,7 +73,6 @@ class GuessingGame
 
   def play
     loop do
-      p @winning_num
       break if @count <= 0
       count
       guess = player_guess
@@ -139,3 +138,121 @@ game.play
 # You have 3 guesses remaining.
 # Enter a number between 1 and 100: 81
 # That's the number!
+
+=begin
+LS Solution
+
+class GuessingGame
+  MAX_GUESSES = 7
+  RANGE = 1..100
+
+  RESULT_OF_GUESS_MESSAGE = {
+    high:  "Your number is too high.",
+    low:   "Your number is too low.",
+    match: "That's the number!"
+  }.freeze
+
+  WIN_OR_LOSE = {
+    high:  :lose,
+    low:   :lose,
+    match: :win
+  }.freeze
+
+  RESULT_OF_GAME_MESSAGE = {
+    win:  "You won!",
+    lose: "You have no more guesses. You lost!"
+  }.freeze
+
+  def initialize
+    @secret_number = nil
+  end
+
+  def play
+    reset
+    game_result = play_game
+    display_game_end_message(game_result)
+  end
+
+  private
+
+  def reset
+    @secret_number = rand(RANGE)
+  end
+
+  def play_game
+    result = nil
+    MAX_GUESSES.downto(1) do |remaining_guesses|
+      display_guesses_remaining(remaining_guesses)
+      result = check_guess(obtain_one_guess)
+      puts RESULT_OF_GUESS_MESSAGE[result]
+      break if result == :match
+    end
+    WIN_OR_LOSE[result]
+  end
+
+  def display_guesses_remaining(remaining)
+    puts
+    if remaining == 1
+      puts 'You have 1 guess remaining.'
+    else
+      puts "You have #{remaining} guesses remaining."
+    end
+  end
+
+  def obtain_one_guess
+    loop do
+      print "Enter a number between #{RANGE.first} and #{RANGE.last}: "
+      guess = gets.chomp.to_i
+      return guess if RANGE.cover?(guess)
+      print "Invalid guess. "
+    end
+  end
+
+  def check_guess(guess_value)
+    return :match if guess_value == @secret_number
+    return :low if guess_value < @secret_number
+    :high
+  end
+
+  def display_game_end_message(result)
+    puts "", RESULT_OF_GAME_MESSAGE[result]
+  end
+end
+
+Discussion
+Our class begins with some constants: the number of guesses allowed per game, the range for the secret number, 
+and three Hashes that we use to print messages without a lot of logic. None of these constants are necessary, but 
+each helps to make our code clearer.
+
+Most classes need an #initialize method to initialize its internal state. That isn't necessary here since we don't 
+need to choose a secret number until play begins. It's good practice, though, to always initialize your instance 
+variables in the #initialize method, even if you don't have to. It provides a single location where you can see all 
+your instance variables.
+
+The #play method handles each round of the game:
+
+It calls #reset to reset the game, that is, generate a new secret number.
+It calls #play_game to play a single round, and returns the result (:win if the player wins, :lose otherwise).
+We use the result to display the game end message with #display_game_end_message.
+#play_game is the meat of the game: it asks the player for each guess, but no more than allowed. If the player 
+guesses the number, the method returns :win. Otherwise, it returns :lose.
+
+The loop in #play_game calls #obtain_one_guess to get a number from the player. This method should be familiar; 
+you've written plenty like it so far in your Launch School experience. The call to #cover? may be unfamiliar; 
+#cover? is a Range method like the familiar #include? method, but is much faster than #include? when working with 
+Ranges.
+
+We also call #check_guess with the guessed number, which determines whether it's equal to, less than, or greater 
+than the secret number, and returns the result as a symbol. We use that symbol in #play_game to print the 
+appropriate result message from RESULT_OF_GUESS_MESSAGE.
+
+#display_guesses_remaining displays the number of guesses remaining.
+
+Further Exploration
+You will need the original exercise solution for the next exercise. If you work on this Further Exploration, keep 
+a copy of your original solution so you can reuse it.
+
+We took a straightforward approach here and implemented a single class. Do you think it's a good idea to have a 
+Player class? What methods and data should be part of it? How many Player objects do you need? Should you use 
+  inheritance, a mix-in module, or a collaborative object?
+=end
