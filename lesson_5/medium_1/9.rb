3b7cdb7b848e50bc3e4ac25c109c9337193e0d77
mskalisz <mskalisz3@gmail.com>
2021-12-23 20:11:04 -0500

    Attempt Deck of Cards

--- lesson_5/medium_1/9.rb
+++ lesson_5/medium_1/9.rb
@@ -28,3 +28,41 @@ very, very old before you see two consecutive shuffles produce the same results.
 almost certainly have something wrong.
 
 =end
+
+class Deck
+  RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  def draw
+  end
+end
+
+class Card
+  include Comparable
+  attr_reader :rank, :suit
+
+  VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def value
+    VALUES.fetch(rank, rank)
+  end
+
+  def <=>(other_card)
+    value <=> other_card.value
+  end
+end
+
+deck = Deck.new
+drawn = []
+52.times { drawn << deck.draw }
+drawn.count { |card| card.rank == 5 } == 4
+drawn.count { |card| card.suit == 'Hearts' } == 13
