0f5642f0a1ada2e6f4c3537360ecf9a7168e2d37
mskalisz <mskalisz3@gmail.com>
2021-12-31 15:43:26 -0500

    Continue to use practice file

--- interview_prep/practice.rb
+++ interview_prep/practice.rb
@@ -1,44 +1,16 @@
-class AnimalClass
-  attr_accessor :name, :animals
+class Person
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @animals = []
   end
 
-  def <<(animal)
-    animals << animal
-  end
-
-  # def +(other_class)
-  #   animals + other_class.animals
-  # end
-
-  def +(other_class)
-    combined = AnimalClass.new("Combined Animals")
-    combined.animals =  animals + other_class.animals
-    combined
+  def change_name
+    name = name.upcase
   end
 end
 
-class Animal
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-  end
-end 
-
-mammals = AnimalClass.new('Mammals')
-mammals << Animal.new('Human')
-mammals << Animal.new('Dog')
-mammals << Animal.new('Cat')
-
-birds = AnimalClass.new('Birds')
-birds << Animal.new('Eagle')
-birds << Animal.new('Blue Jay')
-birds << Animal.new('Penguin')
-
-some_animal_classes = mammals + birds
-
-p some_animal_classes
+bob = Person.new('Bob')
+p bob.name
+bob.change_name
+p bob.name
