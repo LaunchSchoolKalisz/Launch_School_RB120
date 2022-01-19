90a2b1d6212f73db0007c672b25de28e44e1671d
mskalisz <mskalisz3@gmail.com>
2022-01-19 09:59:12 -0500

    Add focus list to own examples

--- interview_prep/own_examples.rb
+++ interview_prep/own_examples.rb
@@ -649,3 +649,69 @@ Team.class_method
   packers.players << arod
   
   p packers
+
+  # For each: How? What? Why? Along with own code example
+  # To focus on especially: Polymorphism, capsulation, collaborator objects
+
+# Classes and Objects
+  # Objects
+  # Classes
+  # Object Instantiation
+  # Instance Variables
+  # Instance Methods
+  # Class Variables
+  # Class Methods
+  
+    # Instance Methods vs Class Methods
+
+# Setter and Getter Methods
+  # Getter Methods
+  # Setter Methods
+  # Using attr_*
+    # Getter method
+    # Setter method
+
+# Method Access Control
+  # Public
+  # Private
+  # Protected
+
+# Inheritance
+  # Class Inheritance
+  # Interface Inheritance
+  # Method Lookup Path
+  # Super
+
+# Polymorphism & Encapsulation
+  # Polymorphism
+    # Polymorphism Through Inheritance
+    # Polymorphism Through Duck Typing
+
+  # Encapsulation
+
+# Modules
+  # Mixin Modules
+  # Namespacing
+  # Module Methods
+
+# Self
+  # Inside Instance Methods
+  # Inside Class Methods
+  # Inside Class Definitions
+  # Inside Mixin Modules
+  # Outside Any Class
+
+# Fake Operators and Equality
+  # Equivalence
+    # ==
+    # equal? and object_id
+    # ===
+    # eql?
+
+  # Fake Operators
+    # Comparison Methods
+    # Right and Left Shift
+    # Plus
+    # Element Setters and Getters
+
+# Collaborator Objects
